577afd895a2c119c8f52ccf1eccb634755c2aca7
MD RIJWAN <md1994rijwan@gmail.com>
2018-11-10 20:39:01 +0530

    Commit 10-Nov-2018

--- Annexure1.py
+++ Annexure1.py
@@ -36,24 +36,37 @@ for udate in tsample_date:
         T_site=t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p)].t_site.unique()
         T_plant=t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p)].t_plant.unique()
         Sampling_point=tu_s_p
-        CH4=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & (t_data.name=='Methane - CH4')].reported_value)
-        H2=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & (t_data.name=='Hydrogen - H2')].reported_value)
-        C2H2=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & (t_data.name=='Acetylene- C2H2')].reported_value)
-        C2H4=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & (t_data.name=='Ethylene - C2H4')].reported_value)
-        C2H6=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & (t_data.name=='Ethane - C2H6')].reported_value)
-        CO=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & (t_data.name=='Carbon Monoxide - CO')].reported_value)
-        CO2=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & (t_data.name=='Carbon Dioxide - CO2')].reported_value)
-        TDCG=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & (t_data.name=='TDCG')].reported_value)
+        CH4=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & 
+                         (t_data.name=='Methane - CH4')].reported_value)
+        H2=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & 
+                        (t_data.name=='Hydrogen - H2')].reported_value)
+        C2H2=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & 
+                          (t_data.name=='Acetylene- C2H2')].reported_value)
+        C2H4=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & 
+                          (t_data.name=='Ethylene - C2H4')].reported_value)
+        C2H6=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & 
+                          (t_data.name=='Ethane - C2H6')].reported_value)
+        CO=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & 
+                        (t_data.name=='Carbon Monoxide - CO')].reported_value)
+        CO2=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & 
+                         (t_data.name=='Carbon Dioxide - CO2')].reported_value)
+        TDCG=float(t_data[(t_data.sampled_date==udate) & (t_data.sampling_point==tu_s_p) & 
+                          (t_data.name=='TDCG')].reported_value)
         if((H2<=100) and (CH4<=120) and (C2H2<=1) and (C2H4<=50) and (C2H6<=65) and (CO<=350) and (CO2<=2500) and (TDCG<=720)):
             condition='Condition 1'
         else:
-            if(((H2>=101) and (H2<=700)) or ((CH4>=121) and (CH4<=400)) or ((C2H2>=2) and (C2H2<=9)) or ((C2H4>=51) and (C2H4<=100)) or ((C2H6>=66) and (C2H6<=100)) or ((CO>=351) and (CO<=570)) or ((CO2>=2500) and (CO2<=4000)) or ((TDCG>=721) and (TDCG<=1920))):
+            if(((H2>=101) and (H2<=700)) or ((CH4>=121) and (CH4<=400)) or ((C2H2>=2) and (C2H2<=9)) or 
+               ((C2H4>=51) and (C2H4<=100)) or ((C2H6>=66) and (C2H6<=100)) or ((CO>=351) and (CO<=570)) or 
+               ((CO2>=2500) and (CO2<=4000)) or ((TDCG>=721) and (TDCG<=1920))):
                 condition='Condition 2'
                 execute=False
-            if(((H2>=701) and (H2<1800)) or ((CH4>=401) and (CH4<1000)) or ((C2H2>=10) and (C2H2<35)) or ((C2H4>=101) and (C2H4<200)) or ((C2H6>=101) and (C2H6<150)) or ((CO>=571) and (CO<1400)) or ((CO2>=4001) and (CO2<10000)) or ((TDCG>=1921) and (TDCG<4630))):
+            if(((H2>=701) and (H2<1800)) or ((CH4>=401) and (CH4<1000)) or ((C2H2>=10) and (C2H2<35)) or 
+               ((C2H4>=101) and (C2H4<200)) or ((C2H6>=101) and (C2H6<150)) or ((CO>=571) and (CO<1400)) or 
+               ((CO2>=4001) and (CO2<10000)) or ((TDCG>=1921) and (TDCG<4630))):
                 condition='Condition 3'
                 execute=False
-            elif((H2>= 1800) or (CH4>=1000) or (C2H2>=35) or (C2H4>=200) or (C2H6>=150) or (CO>=1400) or (CO2>=10000) or (TDCG>=4360)):
+            elif((H2>= 1800) or (CH4>=1000) or (C2H2>=35) or (C2H4>=200) or (C2H6>=150) or (CO>=1400) or 
+                 (CO2>=10000) or (TDCG>=4360)):
                 condition='Condition 4'
                 execute=False
         if(H2>=1000):
